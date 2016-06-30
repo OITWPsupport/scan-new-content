49aa9aff0003de2ece4bf3f8de074e6fb2c361a9
davidmlentz <davidmlentz@gmail.com>
2016-06-30 09:42:24 -0600

    version++

--- scan_new_content.php
+++ scan_new_content.php
@@ -3,7 +3,7 @@
 Plugin Name: Boise State Scan New Content
 Description: Triggers an accessibility scan of each new or updated page and post. Adds an 
 admin menu and form for maintaining associated info.
-Version: 0.1.1
+Version: 0.1.3
 Author: David Lentz
  */
 
